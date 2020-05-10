dfd13dbc514cf0a77ce1423a7a96b14aeee0d70f
Zhenya <zasko180@gmail.com>
2020-05-10 13:36:46 +0300

    add ability to use mysql and create db from code (#54)
    * ER-model minimalized
    
    * Database initialization
    
    * Added Gradle
    
    * Kotlin added
    
    * Types changed
    
    * add node labels table
    
    * Neo4j Connector
    
    * Gradle unignored
    
    * add postgreConnector
    
    * Fixed Relationship
    
    * add an ability to insert data into db
    
    * add ability to use mysql and create db from code
    
    Co-authored-by: ALEGATOR1209 <alekos1209@gmail.com>

--- src/main/kotlin/connectors/PostgreConnector.kt
+++ src/main/kotlin/connectors/PostgreConnector.kt
@@ -1,12 +1,23 @@
 package connectors
 
+import connectors.PostgreConnector.Connections.references
 import models.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class PostgreConnector(user: String, password: String, driver: String, URL: String) {
+    private val schema = Schema("cytrus")
     init {
         Database.connect(URL, driver, user, password)
+        transaction {
+            addLogger(StdOutSqlLogger)
+            with (SchemaUtils) {
+                dropSchema(schema, cascade = true)
+                createSchema(schema)
+                setSchema(schema)
+                create(Nodes, NodeProperties, Connections, ConnectionProperties)
+            }
+        }
     }
 
     object Nodes: Table("nodes") {
@@ -15,34 +26,27 @@ class PostgreConnector(user: String, password: String, driver: String, URL: Stri
     }
 
     object NodeProperties: Table("node_properties") {
-        val nodeId = long("node_id")
+        val nodeId = (long("node_id").references(Nodes.id, ReferenceOption.CASCADE, ReferenceOption.CASCADE))
         val key = varchar("key", 64)
         val value = varchar("value", 512)
     }
 
-    object NodeLabels: Table("node_labels") {
-        val nodeId = long("node_id")
-        val label = varchar("label", 64)
-    }
-
     object Connections: Table("connections") {
         val id = long("id")
-        val targetId = long("target_id")
-        val sourceId = long("source_id")
+        val targetId = (long("target_id").references(Nodes.id, ReferenceOption.CASCADE, ReferenceOption.CASCADE))
+        val sourceId = (long("source_id").references(Nodes.id, ReferenceOption.CASCADE, ReferenceOption.CASCADE))
+        override val primaryKey = PrimaryKey(Connections.id)
     }
 
     object ConnectionProperties: Table("connection_properties") {
-        val connectionId = long("connection_id")
+        val connectionId = (long("connection_id").references(Connections.id, ReferenceOption.CASCADE, ReferenceOption.CASCADE))
         val key = varchar("key", 64)
         val value = varchar("value", 512)
     }
 
     fun saveNodes(nodes: List<NodeData>) {
         transaction {
-
-            val schema = Schema("cytrus")
             SchemaUtils.setSchema(schema)
-            addLogger(StdOutSqlLogger)
 
             for (node in nodes) {
                 Nodes.insert {
@@ -50,9 +54,10 @@ class PostgreConnector(user: String, password: String, driver: String, URL: Stri
                 }
 
                 node.labels.forEach { l ->
-                    NodeLabels.insert {
+                    NodeProperties.insert {
                         it[nodeId] = node.id
-                        it[label] = l
+                        it[key] = "LABEL"
+                        it[value] = l
                     }
                 }
 
@@ -63,15 +68,12 @@ class PostgreConnector(user: String, password: String, driver: String, URL: Stri
                         it[value] = v.toString()
                     }
                 }
-
             }
         }
     }
 
     fun saveRelationships(connections: List<RelationshipData>) {
         transaction {
-            addLogger(StdOutSqlLogger)
-            val schema = Schema("cytrus")
             SchemaUtils.setSchema(schema)
 
             for (connection in connections) {
