c783f5d09f1825342c2a52b3f0874a3aca0f13ac
ALEGATOR1209 <kovalyshinO@gmail.com>
2020-05-10 18:11:14 +0300

    Readme update (#55)
    * Readme updated
    
    * Usage in readme
    
    * Ultra super last finax minor fix typo
    
    * Initial data fixed

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Cytrus
 Service of analytical processing of texts in Ukrainian language
 
+# Інсталяція
+## 1.1 SQL-only
+### 1.1.1 PostgreSQL
+1. Install [PostgreSQL](https://www.postgresql.org/download/);
+2. `CREATE DATABASE cytrus;`
+3. `psql cytrus < init_postgre.sql;`
+4. `psql cytrus < initial_data.sql;`
+
+### 1.1.2 MySQL
+1. Install MySQL;
+2. Run init_my.sql;
+3. Run initial_data.sql.
+
+## 1.2 Import from Neo4j into SQL
+1. Install [Neo4j](https://neo4j.com/download/);
+2. Install [PostgreSQL](https://www.postgresql.org/download/) or MySQL;
+3. Install Gradle, JDK and Kotlin;
+4. Choose right example in [main.kt](/src/main/kotlin/main.kt);
+5. Enter your login, password;
+6. `./gradlew build`;
+7. `./gradlew run`.
+
 # Participants
 * [Засько Євгеній](https://github.com/NeProgramist)
 * [Ковалишин Олег](https://github.com/ALEGATOR1209)
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,14 +3,14 @@ import models.*
 
 fun main() {
     // From Neo4j to MySQL
-   transfer(
-       neo4jLogin = "neo4j",
-       neo4jPassword = "agb",
-       sqlURL = "jdbc:mysql://localhost:3306/",
-       sqlDriver = "com.mysql.jdbc.Driver",
-       sqlLogin = "cytrus",
-       sqlPassword = "agb"
-   )
+//   transfer(
+//       neo4jLogin = "neo4j",
+//       neo4jPassword = "agb",
+//       sqlURL = "jdbc:mysql://localhost:3306/",
+//       sqlDriver = "com.mysql.jdbc.Driver",
+//       sqlLogin = "cytrus",
+//       sqlPassword = "agb"
+//   )
 
     // From Neo4j to PostgreSQL
     transfer(
@@ -23,9 +23,9 @@ fun main() {
     )
 
     // Usage
-    val connector = Neo4jConnector("bolt://localhost:7687", "neo4j", "agb")
-    connector.addNode(NodeData(
-        1000, listOf("Label1", "Label2"), mapOf("key" to "value", "key2" to "value2")
-    ))
-    connector.close()
+//    val connector = Neo4jConnector("bolt://localhost:7687", "neo4j", "agb")
+//    connector.addNode(NodeData(
+//        1000, listOf("Label1", "Label2"), mapOf("key" to "value", "key2" to "value2")
+//    ))
+//    connector.close()
 }
